28c06e51326a44b07051fadd0f4fcd1bb4a7d0d8
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-08-24 14:13:35 +0300

    g

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+// emad
+
+// manar
+
+// laith
+
+// duaa
+
+// jafar
+
+
+
+
+// dropdown start here
+function show(anything){
+ document.querySelector('.textBox').value = anything;
+}
+
+let dropdown = document.querySelector('.dropdown');
+dropdown.onclick = function(){
+dropdown.classList.toggle('active')
+}
+
+// dropdown End here
+
+
+
+
+
