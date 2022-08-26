f63d3d8588c3602b816a92eaf4e95bec16f248bf
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-08-26 12:43:21 +0300

    ,

--- app.js
+++ app.js
@@ -4,72 +4,72 @@
 
 // laith
 function Task(title, description, priority) {
-  this.title = title;
-  this.description = description;
-  this.priority = priority;
-  this.status = "incomplete";
-  this.id = new Date().getTime();
+	this.title = title
+	this.description = description
+	this.priority = priority
+	this.status = 'incomplete'
+	this.id = new Date().getTime()
 }
 
-let allCards = [];
+let allCards = []
 
-let allTasks = [];
+let allTasks = []
 
-let form = document.getElementById("newTaskForm");
+let form = document.getElementById('newTaskForm')
 
 function render(event) {
-  event.preventDefault();
+	event.preventDefault()
 
-  let title = event.target.Title.value;
-  let description = event.target.Description.value;
-  let priority = event.target.Radio.value;
+	let title = event.target.Title.value
+	let description = event.target.Description.value
+	let priority = event.target.Radio.value
 
-  let taskCard = new Task(title, description, priority);
-  allTasks.push(taskCard);
-  saveToLocal();
+	let taskCard = new Task(title, description, priority)
+	allTasks.push(taskCard)
+	saveToLocal()
 
-  addCard(taskCard);
-  document.getElementById("newTaskForm").reset();
+	addCard(taskCard)
+	document.getElementById('newTaskForm').reset()
 }
 
-form.addEventListener("submit", render);
+form.addEventListener('submit', render)
 
-let userName = "laith"; // example for testing, actual username is from manar
+let userName = 'laith' // example for testing, actual username is from manar
 
 function saveToLocal() {
-  let stringArr = JSON.stringify(allTasks);
-  localStorage.setItem(userName, stringArr);
+	let stringArr = JSON.stringify(allTasks)
+	localStorage.setItem(userName, stringArr)
 }
 function getFromLocal() {
-  let jsonArr = localStorage.getItem(userName);
-  let objArr = JSON.parse(jsonArr);
+	let jsonArr = localStorage.getItem(userName)
+	let objArr = JSON.parse(jsonArr)
 
-  if (objArr !== null) {
-    objArr.forEach((element) => {
-      allTasks = objArr;
-      addCard(element);
-    });
-  }
+	if (objArr !== null) {
+		objArr.forEach((element) => {
+			allTasks = objArr
+			addCard(element)
+		})
+	}
 }
-getFromLocal();
+getFromLocal()
 // duaa
-let cardRow = document.getElementById("card-row");
+let cardRow = document.getElementById('card-row')
 // bootstrap tooltip
 let tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
+	document.querySelectorAll('[data-bs-toggle="tooltip"]')
+)
 // let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 // 	return new bootstrap.Tooltip(tooltipTriggerEl);
 // });
 
 // addCard function to add cards from form
-let cardContainer = document.getElementById("card-container");
+let cardContainer = document.getElementById('card-container')
 
 function addCard(task) {
-  const taskCard = document.createElement("div");
-  taskCard.classList.add("col-sm-4");
+	const taskCard = document.createElement('div')
+	taskCard.classList.add('col-sm-4')
 
-  taskCard.innerHTML = `
+	taskCard.innerHTML = `
     <div class="box ${colorClass(task.priority)}" data-bs-toggle="tooltip"
     data-bs-placement="top"
     title="${task.priority}">
@@ -101,50 +101,50 @@ function addCard(task) {
     </a>
     </div>
   
-    `;
+    `
 
-  task.element = taskCard;
-  allCards.push(task);
-  // console.log(allCards)
+	task.element = taskCard
+	allCards.push(task)
+	// console.log(allCards)
 
-  document.getElementById("card-row").appendChild(taskCard);
+	document.getElementById('card-row').appendChild(taskCard)
 
-  // function to delete each element
-  let deleteBtn = document.querySelectorAll(".delete-btn");
-  deleteBtn.forEach((ele) => {
-    ele.addEventListener("click", (e) => {
-      e.preventDefault();
-      console.log(e);
-      let deleteId = e.target.id;
-      console.log(deleteId);
-      console.log(allTasks);
-      allTasks = allTasks.filter((ele) => {
-        return ele.id == deleteId ? false : true;
-      });
-      console.log(allTasks);
-      saveToLocal();
-      // localStorage.setItem(userName, JSON.stringify(allTasks));
-      cardRow.innerHTML = "";
-      getFromLocal();
-      // let jsonArr = localStorage.getItem(userName);
-      // let arr = JSON.parse(jsonArr);
-      // allTasks = arr;
-      // arr.forEach((ele) => {
-      // 	addCard(ele);
-      // });
-    });
-  });
+	// function to delete each element
+	let deleteBtn = document.querySelectorAll('.delete-btn')
+	deleteBtn.forEach((ele) => {
+		ele.addEventListener('click', (e) => {
+			e.preventDefault()
+			console.log(e)
+			let deleteId = e.target.id
+			console.log(deleteId)
+			console.log(allTasks)
+			allTasks = allTasks.filter((ele) => {
+				return ele.id == deleteId ? false : true
+			})
+			console.log(allTasks)
+			saveToLocal()
+			// localStorage.setItem(userName, JSON.stringify(allTasks));
+			cardRow.innerHTML = ''
+			getFromLocal()
+			// let jsonArr = localStorage.getItem(userName);
+			// let arr = JSON.parse(jsonArr);
+			// allTasks = arr;
+			// arr.forEach((ele) => {
+			// 	addCard(ele);
+			// });
+		})
+	})
 }
 // function to change top border color of cards based on priority
 function colorClass(priority) {
-  switch (priority) {
-    case "Critical":
-      return "red";
-    case "Normal":
-      return "orange";
-    case "Low priority":
-      return "blue";
-  }
+	switch (priority) {
+		case 'Critical':
+			return 'red'
+		case 'Normal':
+			return 'orange'
+		case 'Low priority':
+			return 'blue'
+	}
 }
 
 // const editBtn = document.querySelector(".edit-btn");
@@ -155,242 +155,249 @@ function colorClass(priority) {
 
 // jafar
 
-
-
-
-function displayCards(filteredArray) {
-
-  filteredArray.forEach((ele) => {
-    if(ele.style.display == "none"){
-      ele.style.display = "block";
-
-    }else{
-    ele.style.display = "none";
-  }  })
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //when click on dropdown data show it instead of the placeholder "filter"
 
-console.log(allCards);
+console.log(allCards)
 
 function show1(firstDropdownvalue) {
-  document.querySelector(".textBox1").value = firstDropdownvalue;
+	document.querySelector('.textBox1').value = firstDropdownvalue
+}
+var firstCondition = null
+var secondCondition = null
+function getStatus(sel) {
+	firstCondition = sel.options[sel.selectedIndex].text
+
+	let filtered =
+		firstCondition && secondCondition
+			? allCards.filter(
+					(card) =>
+						card.priority == secondCondition && card.status == firstCondition
+			  )
+			: firstCondition
+			? allCards.filter((card) => card.priority == firstCondition)
+			: secondCondition
+			? allCards.filter((card) => card.priority == secondCondition)
+			: allCards
+
+	displayCards(filtered)
+}
+function getPriority(sel) {
+	secondCondition = sel.options[sel.selectedIndex].text
+	console.log(`filteredarray before ${filteredArray}`)
+	let filtered =
+		firstCondition && secondCondition
+			? allCards.filter(
+					(card) =>
+						card.priority == secondCondition && card.status == firstCondition
+			  )
+			: firstCondition
+			? allCards.filter((card) => card.priority == firstCondition)
+			: secondCondition
+			? allCards.filter((card) => card.priority == secondCondition)
+			: allCards
+
+	displayCards(filtered)
+}
+function displayCards(filteredArray) {
+	allCards.forEach((ele) => {
+		ele.element.style.display = 'none'
+	})
+	filteredArray.forEach((ele) => {
+		ele.element.style.display = 'block'
+	})
 }
 
 function show2(secondDropdownvalue) {
-  document.getElementById("TextBox2").value = secondDropdownvalue;
+	document.getElementById('TextBox2').value = secondDropdownvalue
 }
 
 //toggle to class active once we clicked on the dropdown
 
-let dropdown1 = document.getElementById("filterByStatus");
+let dropdown1 = document.getElementById('filterByStatus')
 dropdown1.onclick = function () {
-  dropdown1.classList.toggle("active");
-};
-  
- 
-let dropdown2 = document.getElementById("filterByPriority");
+	dropdown1.classList.toggle('active')
+}
+
+let dropdown2 = document.getElementById('filterByPriority')
 dropdown2.onclick = function () {
-  dropdown2.classList.toggle("active");
-};
+	dropdown2.classList.toggle('active')
+}
 
-let inComplete = document.getElementById("InCompleteDropdown");
-inComplete.addEventListener("click", (event) => {
-  event.preventDefault();
+let inComplete = document.getElementById('InCompleteDropdown')
+var filteredArray = []
+inComplete.addEventListener('click', (event) => {
+	event.preventDefault()
 
-  let otherFilterValue = document.querySelector(".textBox1").value;
-  // let firstCondition = document.querySelector(".textBox1").value;
-  //  let secondCondition = document.querySelector(".textBox2").value;
+	let otherFilterValue = document.querySelector('.textBox1').value
+	firstCondition = document.querySelector('.textBox1').value
+	secondCondition = document.querySelector('.textBox2').value
 
-  
+	filteredArray = allCards.filter((card) => card.priority == otherFilterValue)
 
-  filteredArray = allCards.filter((card) => card.priority == otherFilterValue);
+	console.log(filteredArray.length)
+	console.log(otherFilterValue)
 
-  console.log(filteredArray.length);
-  console.log(otherFilterValue);
+	if (filteredArray.length == 0) {
+		allCards.forEach((card) => {
+			const visible = card.status.toLowerCase() === 'incomplete'
+			if (visible != true) {
+				card.element.style.display = 'none'
+			} else {
+				card.element.style.display = 'block'
+			}
+		})
+	} else {
+		filteredArray.forEach((card) => {
+			const visible = card.status.toLowerCase() === 'incomplete'
+			if (visible === true) {
+				card.element.style.display = 'block'
+			} else {
+				card.element.style.display = 'none'
+			}
+		})
+	}
+})
 
-  if (filteredArray.length == 0) {
-    allCards.forEach((card) => {
-      const visible = card.status.toLowerCase() === "incomplete";
-      if (visible != true) {
-        card.element.style.display = "none";
-      } else {
-        card.element.style.display = "block";
-      }
-    });
-  } else {
-    filteredArray.forEach((card) => {
-      const visible = card.status.toLowerCase() === "incomplete";
-      if (visible === true) {
-        card.element.style.display = "block";
-      } else {
-        card.element.style.display = "none";
-      }
-    });
-  }
-});
+let completedDropdown = document.getElementById('completedDropdown')
 
-let completedDropdown = document.getElementById("completedDropdown");
+completedDropdown.addEventListener('click', (event) => {
+	event.preventDefault()
 
-completedDropdown.addEventListener("click", (event) => {
-  event.preventDefault();
+	let otherFilterValue = document.querySelector('.textBox1').value
+	console.log(allCards.filter((card) => card.priority == otherFilterValue))
+	let filteredArray = allCards.filter(
+		(card) => card.priority == otherFilterValue
+	)
 
-  let otherFilterValue = document.querySelector(".textBox1").value;
-  console.log(allCards.filter((card) => card.priority == otherFilterValue));
-  let filteredArray = allCards.filter(
-    (card) => card.priority == otherFilterValue
-  );
+	console.log(filteredArray.length)
 
-  console.log(filteredArray.length);
+	if (filteredArray.length == 0) {
+		allCards.forEach((card) => {
+			const visible = card.status.toLowerCase() === 'completed'
+			if (visible != true) {
+				card.element.style.display = 'none'
+			} else {
+				card.element.style.display = 'block'
+			}
+		})
+	} else {
+		filteredArray.forEach((card) => {
+			const visible = card.status.toLowerCase() === 'completed'
+			if (visible === true) {
+				card.element.style.display = 'block'
+			} else {
+				card.element.style.display = 'none'
+			}
+		})
+	}
+})
 
-  if (filteredArray.length == 0) {
-    allCards.forEach((card) => {
-      const visible = card.status.toLowerCase() === "completed";
-      if (visible != true) {
-        card.element.style.display = "none";
-      } else {
-        card.element.style.display = "block";
-      }
-    });
-  } else {
-    filteredArray.forEach((card) => {
-      const visible = card.status.toLowerCase() === "completed";
-      if (visible === true) {
-        card.element.style.display = "block";
-      } else {
-        card.element.style.display = "none";
-      }
-    });
-  }
-});
+let CriticalDropdown = document.getElementById('CriticalDropdown')
 
-let CriticalDropdown = document.getElementById("CriticalDropdown");
+CriticalDropdown.addEventListener('click', (event) => {
+	event.preventDefault()
 
-CriticalDropdown.addEventListener("click", (event) => {
-  event.preventDefault();
+	let otherFilterValue = document.querySelector('.textBox2').value
+	console.log(allCards.filter((card) => card.status == otherFilterValue))
+	let filteredArray = allCards.filter((card) => card.status == otherFilterValue)
 
-  let otherFilterValue = document.querySelector(".textBox2").value;
-  console.log(allCards.filter((card) => card.status == otherFilterValue));
-  let filteredArray = allCards.filter(
-    (card) => card.status == otherFilterValue
-  );
+	console.log(filteredArray.length)
 
-  console.log(filteredArray.length);
+	if (filteredArray.length === 0) {
+		allCards.forEach((card) => {
+			const visible = card.priority.toLowerCase() === 'critical'
+			if (visible != true) {
+				card.element.style.display = 'none'
+			} else {
+				card.element.style.display = 'block'
+			}
+		})
+	} else {
+		filteredArray.forEach((card) => {
+			const visible = card.priority.toLowerCase() === 'critical'
+			if (visible === true) {
+				card.element.style.display = 'block'
+			} else {
+				card.element.style.display = 'none'
+			}
+		})
+	}
+})
 
-  if (filteredArray.length === 0) {
-    allCards.forEach((card) => {
-      const visible = card.priority.toLowerCase() === "critical";
-      if (visible != true) {
-        card.element.style.display = "none";
-      } else {
-        card.element.style.display = "block";
-      }
-    });
-  } else {
-    filteredArray.forEach((card) => {
-      const visible = card.priority.toLowerCase() === "critical";
-      if (visible === true) {
-        card.element.style.display = "block";
-      } else {
-        card.element.style.display = "none";
-      }
-    });
-  }
-});
+let MediumDropdown = document.getElementById('MediumDropdown')
 
-let MediumDropdown = document.getElementById("MediumDropdown");
+MediumDropdown.addEventListener('click', (event) => {
+	event.preventDefault()
 
-MediumDropdown.addEventListener("click", (event) => {
-  event.preventDefault();
+	let otherFilterValue = document.querySelector('.textBox2').value
+	console.log(allCards.filter((card) => card.status == otherFilterValue))
+	let filteredArray = allCards.filter((card) => card.status == otherFilterValue)
 
-  let otherFilterValue = document.querySelector(".textBox2").value;
-  console.log(allCards.filter((card) => card.status == otherFilterValue));
-  let filteredArray = allCards.filter(
-    (card) => card.status == otherFilterValue
-  );
+	console.log(filteredArray.length)
 
-  console.log(filteredArray.length);
+	if (filteredArray.length == 0) {
+		allCards.forEach((card) => {
+			const visible = card.priority.toLowerCase() === 'normal'
+			if (visible != true) {
+				card.element.style.display = 'none'
+			} else {
+				card.element.style.display = 'block'
+			}
+		})
+	} else {
+		filteredArray.forEach((card) => {
+			const visible = card.priority.toLowerCase() === 'normal'
+			if (visible === true) {
+				card.element.style.display = 'block'
+			} else {
+				card.element.style.display = 'none'
+			}
+		})
+	}
+})
 
-  if (filteredArray.length == 0) {
-    allCards.forEach((card) => {
-      const visible = card.priority.toLowerCase() === "normal";
-      if (visible != true) {
-        card.element.style.display = "none";
-      } else {
-        card.element.style.display = "block";
-      }
-    });
-  } else {
-    filteredArray.forEach((card) => {
-      const visible = card.priority.toLowerCase() === "normal";
-      if (visible === true) {
-        card.element.style.display = "block";
-      } else {
-        card.element.style.display = "none";
-      }
-    });
-  }
-});
+let LowPrioritDropdown = document.getElementById('LowPrioritDropdown')
 
-let LowPrioritDropdown = document.getElementById("LowPrioritDropdown");
+LowPrioritDropdown.addEventListener('click', (event) => {
+	event.preventDefault()
 
-LowPrioritDropdown.addEventListener("click", (event) => {
-  event.preventDefault();
+	let otherFilterValue = document.querySelector('.textBox2').value
+	console.log(allCards.filter((card) => card.status == otherFilterValue))
+	let filteredArray = allCards.filter((card) => card.status == otherFilterValue)
 
-  let otherFilterValue = document.querySelector(".textBox2").value;
-  console.log(allCards.filter((card) => card.status == otherFilterValue));
-  let filteredArray = allCards.filter(
-    (card) => card.status == otherFilterValue
-  );
+	console.log(filteredArray.length)
 
-  console.log(filteredArray.length);
+	if (filteredArray.length == 0) {
+		allCards.forEach((card) => {
+			const visible = card.priority.toLowerCase() === 'low priority'
+			if (visible != true) {
+				card.element.style.display = 'none'
+			} else {
+				card.element.style.display = 'block'
+			}
+		})
+	} else {
+		filteredArray.forEach((card) => {
+			const visible = card.priority.toLowerCase() === 'low priority'
+			if (visible === true) {
+				card.element.style.display = 'block'
+			} else {
+				card.element.style.display = 'none'
+			}
+		})
+	}
+})
 
-  if (filteredArray.length == 0) {
-    allCards.forEach((card) => {
-      const visible = card.priority.toLowerCase() === "low priority";
-      if (visible != true) {
-        card.element.style.display = "none";
-      } else {
-        card.element.style.display = "block";
-      }
-    });
-  } else {
-    filteredArray.forEach((card) => {
-      const visible = card.priority.toLowerCase() === "low priority";
-      if (visible === true) {
-        card.element.style.display = "block";
-      } else {
-        card.element.style.display = "none";
-      }
-    });
-  }
-});
+let RefreshFilter1 = document.getElementById('RefreshFilter1')
 
-let RefreshFilter1 = document.getElementById("RefreshFilter1");
+RefreshFilter1.addEventListener('click', (event) => {
+	event.preventDefault()
+	location.reload()
+})
 
-RefreshFilter1.addEventListener("click", (event) => {
-  event.preventDefault();
-  location.reload();
-});
+let RefreshFilter2 = document.getElementById('RefreshFilter2')
 
-let RefreshFilter2 = document.getElementById("RefreshFilter2");
-
-RefreshFilter2.addEventListener("click", (event) => {
-  event.preventDefault();
-  location.reload();
-});
+RefreshFilter2.addEventListener('click', (event) => {
+	event.preventDefault()
+	location.reload()
+})
